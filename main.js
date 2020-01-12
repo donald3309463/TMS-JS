1775ee12709f026c941bc65e7ce33f38d8bd8cf3
Donald3309463 <3309463@gmail.com>
2020-01-12 18:12:31 +0300

    JS HW 4

---
+++ main.js
@@ -0,0 +1,52 @@
+//const data = [
+//    {
+//        firstName: 'Ashton',
+//        lastName: 'Kutcher',
+//        age: 40,
+//    },
+//    {
+//        firstName: 'Bradley',
+//        lastName: 'Pitt',
+//        age: 54,
+//    },
+//    {
+//        firstName: 'Hannah',
+//        lastName: 'Dakota',
+//        age: 24,
+//    },
+//];
+
+
+
+
+//for (let i = 0; i <= arr.length; i++) {
+//    if (arr[i] === 4) {
+//        arr[i] = 'four';
+//    }
+//   console.log(arr[i]);
+//}
+//let i = 0;
+//do {
+//    console.log('rrr');
+//    console.log(arr[i]);
+//    i++;
+//} while (i < arr.length);
+//console.log(arr);
+
+// const obj = {
+//     name: 'John',
+//     lastName: 'Smith',
+//     age: 24,
+// };
+// for (let key in obj) {
+//     console.log (`${key}: ${obj[key]}`);
+// }
+
+function min(a, b) {
+    if (a < b) {
+        return a;
+    }
+    else {
+        return b;
+    }
+}
